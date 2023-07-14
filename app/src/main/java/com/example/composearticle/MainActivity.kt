6d0c30869075d1c61b8d856b61ba2ab485194605
myka1 <msmykolas@gmail.com>
2023-07-14 15:56:15 +0300

    Adding Image

---
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Article(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+    ) {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.FillWidth)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArticlePreview() {
+    ComposeArticleTheme {
+        Article()
+    }
+}

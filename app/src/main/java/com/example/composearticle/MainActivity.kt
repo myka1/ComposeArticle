12bb70f916a3ac5517d893711eed3f4570d4db5e
myka1 <msmykolas@gmail.com>
2023-07-14 16:23:01 +0300

    Adding text

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,7 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Article(
+                        title = stringResource(R.string.title),
+                        firstParagraph = stringResource(R.string.first_paragraph),
+                        secondParagraph = stringResource(R.string.second_paragraph)
+                    )
                 }
             }
         }
@@ -35,13 +44,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Article(modifier: Modifier = Modifier) {
+fun Article(
+    title: String,
+    firstParagraph: String,
+    secondParagraph: String,
+    modifier: Modifier = Modifier
+) {
     val image = painterResource(R.drawable.bg_compose_background)
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         modifier = Modifier
     ) {
         Image(painter = image, contentDescription = null, contentScale = ContentScale.FillWidth)
+        Text(text = title, fontSize = 24.sp, modifier = Modifier.padding(16.dp))
+        Text(
+            text = firstParagraph,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = secondParagraph,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
@@ -49,6 +74,10 @@ fun Article(modifier: Modifier = Modifier) {
 @Composable
 fun ArticlePreview() {
     ComposeArticleTheme {
-        Article()
+        Article(
+            title = stringResource(R.string.title),
+            firstParagraph = stringResource(R.string.first_paragraph),
+            secondParagraph = stringResource(R.string.second_paragraph)
+        )
     }
 }
